cb2d3a28b7b720068ada0108ac85ca194d93495b
Yasaswini <yasaswini5kk@gmail.com>
2026-06-18 12:43:41 +0530

    ETL dag updated

--- airflow/dags/etl_dag.py
+++ airflow/dags/etl_dag.py
@@ -1,3 +1,38 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-print("ETL DAG Loaded")
+# Task Functions
+def extract():
+    print("Extracting data from APIs...")
+
+def transform():
+    print("Transforming data...")
+
+def load():
+    print("Loading data into PostgreSQL...")
+
+# DAG Definition
+with DAG(
+    dag_id="enterprise_etl_pipeline",
+    start_date=datetime(2025, 1, 1),
+    schedule="@daily",
+    catchup=False,
+) as dag:
+
+    extract_task = PythonOperator(
+        task_id="extract_data",
+        python_callable=extract
+    )
+
+    transform_task = PythonOperator(
+        task_id="transform_data",
+        python_callable=transform
+    )
+
+    load_task = PythonOperator(
+        task_id="load_data",
+        python_callable=load
+    )
+
+    extract_task >> transform_task >> load_task
