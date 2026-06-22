f698ede871f315d81b5aef0a9c96c1c7322fc17b
Yasaswini <yasaswini5kk@gmail.com>
2026-06-22 11:39:19 +0530

    Integrated extraction module and improved Airflow DAG

--- airflow/dags/etl_dag.py
+++ airflow/dags/etl_dag.py
@@ -1,16 +1,32 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
+from airflow.providers.standard.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+"""
+Enterprise ETL Pipeline
+
+Workflow:
+Extract -> Transform -> Load
+
+Author: Team 1
+"""
+
+# Default Settings
+default_args = {
+    "owner": "team1",
+    "retries": 2,
+    "retry_delay": timedelta(minutes=1)
+}
 
 # Task Functions
 def extract():
-    print("Extracting data from APIs...")
+    print("Extracting data from Salesforce, Stripe and Zendesk APIs...")
 
 def transform():
-    print("Transforming data...")
+    print("Transforming and cleaning data...")
 
 def load():
-    print("Loading data into PostgreSQL...")
+    print("Loading data into SQLite database...")
 
 # DAG Definition
 with DAG(
@@ -18,21 +34,23 @@ with DAG(
     start_date=datetime(2025, 1, 1),
     schedule="@daily",
     catchup=False,
+    default_args=default_args,
+    description="Enterprise ETL Pipeline using Apache Airflow"
 ) as dag:
 
     extract_task = PythonOperator(
-        task_id="extract_data",
+        task_id="extract_salesforce_stripe_zendesk_data",
         python_callable=extract
     )
 
     transform_task = PythonOperator(
-        task_id="transform_data",
+        task_id="transform_clean_data",
         python_callable=transform
     )
 
     load_task = PythonOperator(
-        task_id="load_data",
+        task_id="load_data_to_sqlite",
         python_callable=load
     )
 
-    
+    extract_task >> transform_task >> load_task
